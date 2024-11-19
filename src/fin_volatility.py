a59ddaf4781db906868f3a3e732890a8f9dea04c
Gaston Woollands <g.woollands@gmail.com>
2024-11-19 18:34:02 +0100

    fix: volatility fix

--- src/fin_volatility.py
+++ src/fin_volatility.py
@@ -7,7 +7,7 @@ from .fin_options import implied_volatility, get_greeks
 
 #------------------------------------------------------------------------------------------------
 
-def calculate_hist_vol(df: pd.DataFrame, periods: int = 252):
+def get_hist_vol(df: pd.DataFrame, periods: int = 252):
     """Calculates daily and annualized historical volatility."""
     historical_vol_daily = df.iloc[-60:]["log_return"].std()
     historical_vol_yearly = historical_vol_daily * np.sqrt(periods)
@@ -15,7 +15,7 @@ def calculate_hist_vol(df: pd.DataFrame, periods: int = 252):
 
 #------------------------------------------------------------------------------------------------
 
-def calculate_volatility_expiration(
+def get_option_metrics(
     stock, expiration: str, S: float, historical_vol_daily: float, 
     predicted_volatility: np.array, predicted_volatility_gru: np.array, 
     op_type: str = "P", r: float = 0.0256, periods: int = 252
@@ -60,18 +60,18 @@ def calculate_volatility_expiration(
 
 #------------------------------------------------------------------------------------------------
 
-def calculate_volatilities(
+def get_option_metrics_expirations(
     stock, df: pd.DataFrame, predicted_volatility: np.array, 
     predicted_volatility_gru: np.array, op_type: str = "P", r: float = 0.0256, periods: int = 252
 ):
     """Calculates volatilities for multiple expiration dates."""
     S = df.Close.iloc[-1].item()
-    historical_vol_daily, _ = calculate_hist_vol(df, periods)
+    historical_vol_daily, _ = get_hist_vol(df, periods)
     expirations = list(stock.options)[:30]
 
     results = []
     for expiration in expirations:
-        result = calculate_volatility_expiration(
+        result = get_option_metrics(
             stock, expiration, S, historical_vol_daily, predicted_volatility, 
             predicted_volatility_gru, op_type=op_type, r=r, periods=periods
         )
@@ -83,65 +83,67 @@ def calculate_volatilities(
 
 #------------------------------------------------------------------------------------------------
 
-def calculate_implied_vol_expiration_byma(
+def get_option_metrics_byma(
     expiration: Union[str, pd.Timestamp], S: float, init_date: Union[str, pd.Timestamp], periods: int, 
     op_type: str, r: float, predicted_volatility: np.array, predicted_volatility_gru: np.array,
     data_options: pd.DataFrame ):
 
     """Calculates implied and predicted volatilities for a single expiration."""
-    init_date = pd.to_datetime(init_date)
-    expiration = pd.to_datetime(expiration)
+
+    init_date, expiration = pd.to_datetime(init_date), pd.to_datetime(expiration)
+
     days_to_expiration = (expiration - init_date).days
     if days_to_expiration <= 0:
         return None
-
-    calls = get_date_options_byma(data_options, expiration, "C")
-    puts = get_date_options_byma(data_options, expiration, "P")
+    
     _T = days_to_expiration / periods
 
-    atm_call, atm_put = atm_option(calls, puts, S)
+    options = filter_options_byma(data_options, expiration, op_type)
 
-    if op_type == "C" and atm_call is not None:
-        K = atm_call.strike.item()
-        market_price = atm_call.lastPrice.item()
-        
-    elif op_type == "P" and atm_put is not None:
-        K = atm_put.strike.item()
-        market_price = atm_put.lastPrice.item()
-    else:
+    atm_call, atm_put = atm_option(calls=options if op_type == "C" else None, 
+                                   puts=options if op_type == "P" else None, 
+                                   S=S)
+
+    atm_option_data = atm_call if op_type == "C" else atm_put
+    
+    if atm_option_data is None:
         print(f"No ATM option found for {expiration}")
         return None
 
+    K, market_price = atm_option_data.strike.item(), atm_option_data.close.item() if atm_option_data.close.item() !=0 else atm_option_data.lastPrice.item()
+
     try:
         imp_vol = implied_volatility(S, K, _T, r, market_price, op_type=op_type, periods=periods) * 100
-        pred_vol = predicted_volatility[days_to_expiration - 1].item()
-        pred_vol_gru = predicted_volatility_gru[days_to_expiration - 1].item()
 
+        index = days_to_expiration - 1
+
+        pred_vol, pred_vol_gru = predicted_volatility[index].item(), predicted_volatility_gru[index].item()
         delta, gamma, vega, theta, rho = get_greeks(op_type, S, K, _T, r, imp_vol / 100)
 
+        # Compile metrics
         return {
             "date": expiration.strftime('%Y-%m-%d'),
-            "market_price": S,
+            "asset_price": S,
             "option_type": op_type,
             "strike": K,
             "option_price": market_price,
-            "imp_vol": round(float(imp_vol), 4),
-            "pred_vol": round(float(pred_vol), 4),
-            "pred_vol_gru": round(float(pred_vol_gru), 4),
-            "delta": round(float(delta), 6) ,
-            "gamma": round(float(gamma), 6),
-            "vega": round(float(vega), 6),
-            "theta": round(float(theta), 6),
-            "rho": round(float(rho), 6)
+            "imp_vol": float(round(imp_vol, 4)),
+            "pred_vol": float(round(pred_vol, 4)),
+            "pred_vol_gru": float(round(pred_vol_gru, 4)),
+            "delta": float(round(delta, 6)),
+            "gamma": float(round(gamma, 6)),
+            "vega": float(round(vega, 6)),
+            "theta": float(round(theta, 6)),
+            "rho": float(round(rho, 6)),
         }
-    
+
     except Exception as e:
         print(f"Error processing expiration {expiration}: {e}")
         return None
 
 #------------------------------------------------------------------------------------------------
 
-def calculate_vol_results_byma(
+def get_option_metrics_expirations_byma(
     data_op: pd.DataFrame, df: pd.DataFrame, predicted_volatility: np.array, 
     predicted_volatility_gru: np.array, op_type: str = "P", r: float = 0.0256, periods: int = 252
 ):
@@ -150,11 +152,11 @@ def calculate_vol_results_byma(
     expirations = sorted(list(set(data_op.expiration)))
 
     S = df.Close.iloc[-1].item()
-    vol_daily_hist, _ = calculate_hist_vol(df, periods)
+    vol_daily_hist, _ = get_hist_vol(df, periods)
 
     results = []
     for expiration in expirations:
-        result = calculate_implied_vol_expiration_byma(
+        result = get_option_metrics_byma(
             expiration, S, today, periods, op_type, r, predicted_volatility, 
             predicted_volatility_gru, data_op
         )
@@ -167,10 +169,13 @@ def calculate_vol_results_byma(
 
 #------------------------------------------------------------------------------------------------
 
-def get_date_options_byma(data_options: pd.DataFrame, expiration, option_type: str):
+def filter_options_byma(data_options: pd.DataFrame, expiration, option_type: str):
+    
     """Filters options by expiration date and type."""
+
     if isinstance(expiration, str):
         expiration = pd.to_datetime(expiration)
+
     data_options['expiration'] = pd.to_datetime(data_options['expiration'])
     filtered_options = data_options[(data_options['expiration'] == expiration) & (data_options['option_type'] == option_type)].reset_index(drop=True)
     
